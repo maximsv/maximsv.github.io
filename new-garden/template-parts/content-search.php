c3b63216c1a433693b0ab610828e9826b0cd6c56
Unknown <maximsv1701@gmail.com>
2018-10-24 13:51:12 +0300

    delete site

--- new-garden/template-parts/content-search.php
+++
@@ -1,9 +0,0 @@
- <article id="post-<?php the_ID(); ?>" <?php post_class('blog_post'); ?> >
-	
-	<h4> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h4>
-		
-	<div class="blog_post_img">
-		<a href="<?php the_permalink(); ?>"> <?php the_post_thumbnail(); ?> </a>
-	</div>
-					
-</article>
